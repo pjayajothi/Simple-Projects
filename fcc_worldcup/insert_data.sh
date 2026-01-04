30aa1681e2e4136a106b9572f1678778bef708e7
pjayajothi <PUJAA.JAYAJOTHI@GMAIL.COM>
2026-01-04 14:10:13 -0500

    Update insert_data.sh

--- fcc_worldcup/insert_data.sh
+++ fcc_worldcup/insert_data.sh
@@ -1,3 +1,13 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
 echo $($PSQL "TRUNCATE games,teams")
 
 cat games.csv | while IFS="," read -r YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
